6cfa272917fda7207d3e5879d6e1fe28f62b463f
Marcel Ribeiro <marcelserraribeiro@gmail.com>
2016-01-29 21:37:23 +0100

    Setting up the Server to serve the client (It's working)

--- server/app.js
+++ server/app.js
@@ -5,14 +5,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -20,10 +15,43 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
 app.use('/users', users);
+
+
+if (isDevEnvironment()) {
+  /**
+   * Development Settings
+   */
+  app.use(express.static(path.join(__dirname, '../client')));
+  app.use(express.static(path.join(__dirname, '../client/.tmp')));
+  app.use(express.static(path.join(__dirname, '../client/app')));
+
+  // development error handler
+  // will print stacktrace
+  //app.use(function (err, req, res, next) {
+  //  res.status(err.status || 500);
+  //  res.render('error', {
+  //    message: err.message,
+  //    error: err
+  //  });
+  //});
+} else {
+  /**
+   * Production Settings
+   */
+  app.use(express.static(path.join(__dirname, '/dist')));
+
+  // production error handler
+  // no stacktraces leaked to user
+  //app.use(function (err, req, res, next) {
+  //  res.status(err.status || 500);
+  //  res.render('error', {
+  //    message: err.message,
+  //    error: {}
+  //  });
+  //});
+
+}
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -32,29 +60,11 @@ app.use(function (req, res, next) {
   next(err);
 });
 
-// error handlers
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
+function isDevEnvironment() {
+  console.log('Running Express as %s environment!', app.get('env'));
+  return app.get('env') === 'DEV';
 }
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function (err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
-
 module.exports = app;
+
