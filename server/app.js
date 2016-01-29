7e6caf9087d38c0549b40f3f81d49789a862147f
Marcel Ribeiro <marcelserraribeiro@gmail.com>
2016-01-29 22:09:01 +0100

    Setting up the Server to serve the client (It's working)

--- server/app.js
+++ server/app.js
@@ -5,52 +5,29 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var users = require('./routes/users');
-
 var app = express();
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use('/users', users);
 
 
+//Loads the application from different locations depending on the environment (DEV or PRD)
 if (isDevEnvironment()) {
-  /**
-   * Development Settings
-   */
+  app.use(favicon(path.join(__dirname, '../client/app', 'favicon.ico')));
   app.use(express.static(path.join(__dirname, '../client')));
   app.use(express.static(path.join(__dirname, '../client/.tmp')));
   app.use(express.static(path.join(__dirname, '../client/app')));
-
-  // development error handler
-  // will print stacktrace
-  //app.use(function (err, req, res, next) {
-  //  res.status(err.status || 500);
-  //  res.render('error', {
-  //    message: err.message,
-  //    error: err
-  //  });
-  //});
 } else {
-  /**
-   * Production Settings
-   */
+  app.use(favicon(path.join(__dirname, '/dist', 'favicon.ico')));
   app.use(express.static(path.join(__dirname, '/dist')));
+}
 
-  // production error handler
-  // no stacktraces leaked to user
-  //app.use(function (err, req, res, next) {
-  //  res.status(err.status || 500);
-  //  res.render('error', {
-  //    message: err.message,
-  //    error: {}
-  //  });
-  //});
 
+function isDevEnvironment() {
+  console.log('Running Express as %s environment!', app.get('env'));
+  return app.get('env') === 'DEV';
 }
 
 // catch 404 and forward to error handler
@@ -60,11 +37,6 @@ app.use(function (req, res, next) {
   next(err);
 });
 
-
-function isDevEnvironment() {
-  console.log('Running Express as %s environment!', app.get('env'));
-  return app.get('env') === 'DEV';
-}
 
 module.exports = app;
 
