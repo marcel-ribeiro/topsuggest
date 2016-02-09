3d6b86d9c924824d7ca36f33d7a8d3045cccfbc5
Marcel Ribeiro <marcelserraribeiro@gmail.com>
2016-02-09 22:40:26 +0100

    Design Footer

--- client/app/scripts/directives/footer.directive.js
+++ client/app/scripts/directives/footer.directive.js
@@ -1,11 +1,23 @@
 'use strict';
-angular.module('shared.layout.footer', [])
+
+/**
+ * @ngdoc function
+ * @name clientApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the clientApp
+ */
+angular.module('shared.layout.footer', ['controllers.layout.footer'])
 
   .directive('footerDirective', function () {
     var footerDirective = {
       restrict: 'A',
+      scope: true,
       replace: true,
+      controller: 'footerController',
       templateUrl: 'views/directives/footer.html'
     };
+
+
     return footerDirective;
   });
