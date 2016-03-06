6ff49f6b1f388615e6c467e250e572fbf460bbbc
Marcel Ribeiro <marcelserraribeiro@gmail.com>
2016-03-06 11:45:07 +0100

    Adapting grunt to include html files inside of the script folder

--- client/Gruntfile.js
+++ client/Gruntfile.js
@@ -376,7 +376,7 @@ module.exports = function (grunt) {
           usemin: 'scripts/scripts.js'
         },
         cwd: '<%= yeoman.app %>',
-        src: 'views/{,*/}*.html',
+        src: 'scripts/**/{,*/}*.html',
         dest: '.tmp/templateCache.js'
       }
     },
