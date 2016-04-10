32157d35a6a9693cc3faa2e4522b8ef0297a6b89
Marcel Ribeiro <marcelserraribeiro@gmail.com>
2016-04-10 20:38:28 +0200

    Cleaning up and renaming the Dimensions component to DeviceType

---
+++ client/app/components/shared/device-type/device-type.constants.js
@@ -0,0 +1,12 @@
+/**
+ * @ngdoc
+ * @name topsuggest..device-type.constants
+ * @description "description of the component in markdown"
+ */
+'use strict';
+angular.module('app.components.shared.device-type.constants', [])
+  .constant("DEVICE_TYPE", {
+    "DESKTOP": "desktop",
+    "TABLET": "tablet",
+    "MOBILE": "mobile"
+  });
--- client/app/components/shared/device-type/device-type.controller.js
+++ client/app/components/shared/device-type/device-type.controller.js
@@ -1,27 +1,20 @@
 /**
  * @ngdoc
- * @name app.components.shared.dimensions.controller
+ * @name app.components.shared.device-type.controller
  * @description "This service is used to retrieve the type of media used to access the app (desktop, tablet or mobile)"
  */
 'use strict';
-angular.module('app.components.shared.dimensions.controller', [])
-  .controller('DimensionsController', ['screenSize', 'MEDIAS', DimensionsController])
+angular.module('app.components.shared.device-type.controller', [])
+  .controller('DeviceTypeController', ['screenSize', 'DEVICE_TYPE', DeviceTypeController]);
 
-
-  .constant("MEDIAS", {
-    "DESKTOP": "desktop",
-    "TABLET": "tablet",
-    "MOBILE": "mobile"
-  });
-
-function DimensionsController(screenSize, MEDIAS) {
+function DeviceTypeController(screenSize, DEVICE_TYPE) {
   // Will fire every time the screen is resized
   screenSize.when('lg, md, sm, xs', function () {
     updateMediaVariable();
   });
 
   var vm = this;
-  vm.media = MEDIAS.DESKTOP;
+  vm.media = DEVICE_TYPE.DESKTOP;
   vm.isDesktop = false;
   vm.isTablet = false;
   vm.isMobile = false;
@@ -33,7 +26,7 @@ function DimensionsController(screenSize, MEDIAS) {
     /* Large desktop */
     /* Portrait tablet to landscape and desktop */
     if (screenSize.is('lg, md')) {
-      vm.media = MEDIAS.DESKTOP;
+      vm.media = DEVICE_TYPE.DESKTOP;
       vm.isDesktop = true;
       vm.isTablet = false;
       vm.isMobile = false;
@@ -41,7 +34,7 @@ function DimensionsController(screenSize, MEDIAS) {
 
     /* Landscape phone to portrait tablet */
     else if (screenSize.is('sm')) {
-      vm.media = MEDIAS.TABLET;
+      vm.media = DEVICE_TYPE.TABLET;
       vm.isDesktop = false;
       vm.isTablet = true;
       vm.isMobile = false;
@@ -49,7 +42,7 @@ function DimensionsController(screenSize, MEDIAS) {
 
     /* Landscape phones and down */
     else if (screenSize.is('xs')) {
-      vm.media = MEDIAS.MOBILE;
+      vm.media = DEVICE_TYPE.MOBILE;
       vm.isDesktop = false;
       vm.isTablet = false;
       vm.isMobile = true;
